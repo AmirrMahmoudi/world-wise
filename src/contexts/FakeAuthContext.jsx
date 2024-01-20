51698257aa2c3998a0e6a657caee2addbce56464
AmirrMahmoudi <amirhosssein.mahmoudi@gmail.com>
2024-01-20 13:49:00 +0330

    Adding Fake Authentication Implementing Login

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -58,6 +58,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider ");
+  return context;
 }
 
 export { AuthProvider, useAuth };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -18,7 +18,7 @@ const Homepage = () => {
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
         </h2>
-        <Link to={"/app"} className="cta">
+        <Link to={"/login"} className="cta">
           Start tracking now
         </Link>
       </section>
